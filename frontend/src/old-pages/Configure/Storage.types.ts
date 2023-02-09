7c81e76fff70bb03ecbbd8cf4aea79813eb7a1f3
Alessandro Menduni <mendaomn@amazon.com>
2023-02-09 17:15:35 +0100

    Fix cluster update crashing when the cluster has a storage attached (#27)
    * Fix typo in filename
    
    * Add types for SharedStorage section of the cluster config
    
    * Add mapper to import the SharedStorage section from template into the UI
    
    * Init the uiSettings array when importing a cluster configuration

--- frontend/src/old-pages/Configure/Storage.types.ts
+++ frontend/src/old-pages/Configure/Storage.types.ts
@@ -33,11 +33,105 @@ export const STORAGE_TYPE_PROPS = {
 
 export type StorageType = keyof typeof STORAGE_TYPE_PROPS
 
-export type Storages = {
+export interface EbsSettings {
+  VolumeType?: string
+  Iops?: number
+  Size?: number
+  Encrypted?: boolean
+  KmsKeyId?: string
+  SnapshotId?: string
+  Throughput?: number
+  VolumeId?: string
+  DeletionPolicy?: string
+  Raid?: {
+    Type: string
+    NumberOfVolumes?: number
+  }
+}
+
+export interface EfsSettings {
+  Encrypted?: boolean
+  KmsKeyId?: string
+  EncryptionInTransit?: boolean
+  IamAuthorization?: boolean
+  PerformanceMode?: string
+  ThroughputMode?: string
+  ProvisionedThroughput: number
+  FileSystemId?: string
+  DeletionPolicy?: string
+}
+
+/**
+ * Please note: Some of the properties cannot coexist
+ * see https://docs.aws.amazon.com/parallelcluster/latest/ug/SharedStorage-v3.html#SharedStorage-v3-FsxLustreSettings
+ */
+export interface FsxLustreSettings {
+  StorageCapacity: number
+  DeploymentType?: string
+  ImportedFileChunkSize?: number
+  DataCompressionType?: string
+  ExportPath?: string
+  ImportPath?: string
+  WeeklyMaintenanceStartTime?: string
+  AutomaticBackupRetentionDays?: number
+  CopyTagsToBackups?: boolean
+  DailyAutomaticBackupStartTime?: string
+  PerUnitStorageThroughput?: number
+  BackupId?: string
+  KmsKeyId?: string
+  FileSystemId?: string
+  AutoImportPolicy?: string
+  DriveCacheType?: string
+  StorageType?: string
+  DeletionPolicy?: string
+}
+
+export interface FsxOnTapSettings {
+  VolumeId: string
+}
+
+export interface FsxOpenZfsSettings {
+  VolumeId: string
+}
+
+interface CommonSharedStorageDetails {
   Name: string
-  StorageType: StorageType
   MountDir: string
-}[]
+}
+
+export interface EbsStorage extends CommonSharedStorageDetails {
+  StorageType: 'Ebs'
+  EbsSettings?: EbsSettings
+}
+
+export interface EfsStorage extends CommonSharedStorageDetails {
+  StorageType: 'Efs'
+  EfsSettings?: EfsSettings
+}
+
+export interface FsxLustreStorage extends CommonSharedStorageDetails {
+  StorageType: 'FsxLustre'
+  FsxLustreSettings?: FsxLustreSettings
+}
+
+export interface FsxOnTapStorage extends CommonSharedStorageDetails {
+  StorageType: 'FsxOnTap'
+  FsxOnTapSettings?: FsxOnTapSettings
+}
+
+export interface FsxOpenZfsStorage extends CommonSharedStorageDetails {
+  StorageType: 'FsxOpenZsf'
+  FsxOpenZfsSettings?: FsxOpenZfsSettings
+}
+
+export type Storage =
+  | EbsStorage
+  | EfsStorage
+  | FsxLustreStorage
+  | FsxOnTapStorage
+  | FsxOpenZfsStorage
+
+export type Storages = Storage[]
 
 export type UIStorageSettings = {
   useExisting: boolean
