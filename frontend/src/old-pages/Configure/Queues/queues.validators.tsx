d8118fe502e12861ca41490c5fb9d9f8f0abe3a4
tsscarla <tsscarla@amazon.com>
2023-03-02 14:30:43 +0100

    Align PC validators in validateQueueName

--- frontend/src/old-pages/Configure/Queues/queues.validators.tsx
+++ frontend/src/old-pages/Configure/Queues/queues.validators.tsx
@@ -1,5 +1,8 @@
+export const QUEUE_NAME_MAX_LENGTH = 25
 export const queueNameErrorsMapping = {
   empty: 'wizard.queues.validation.emptyName',
+  max_length: 'wizard.queues.validation.maxLengthName',
+  forbidden_chars: 'wizard.queues.validation.forbiddenCharsName',
 }
 type QueueNameErrorKind = keyof typeof queueNameErrorsMapping
 
@@ -8,6 +11,12 @@ export function validateQueueName(
 ): [boolean, QueueNameErrorKind?] {
   if (!name) {
     return [false, 'empty']
+  }
+  if (name.length > QUEUE_NAME_MAX_LENGTH) {
+    return [false, 'max_length']
+  }
+  if (!/^([a-z][a-z0-9-]+)$/.test(name)) {
+    return [false, 'forbidden_chars']
   }
   return [true]
 }
