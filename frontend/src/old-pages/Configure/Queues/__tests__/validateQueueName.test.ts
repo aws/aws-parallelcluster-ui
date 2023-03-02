209a6bef9762047910867271c71838d1f0e0c04d
tsscarla <tsscarla@amazon.com>
2023-03-02 14:30:43 +0100

    Align PC validators in validateQueueName

--- frontend/src/old-pages/Configure/Queues/__tests__/validateQueueName.test.ts
+++ frontend/src/old-pages/Configure/Queues/__tests__/validateQueueName.test.ts
@@ -1,16 +1,39 @@
 import {validateQueueName} from '../queues.validators'
 
 describe('Given a queue name', () => {
-  describe("when it's defined", () => {
-    it('should be validated', () => {
-      const queueName = 'Queue 0'
-      expect(validateQueueName(queueName)).toEqual([true])
-    })
-  })
-
   describe("when it's blank", () => {
     it('should fail the validation', () => {
       expect(validateQueueName('')).toEqual([false, 'empty'])
+    })
+  })
+
+  describe("when it's more than 25 chars", () => {
+    it('should fail the validation', () => {
+      expect(validateQueueName(new Array(27).join('a'))).toEqual([
+        false,
+        'max_length',
+      ])
+    })
+  })
+
+  describe("when it's less than or equal to 25 chars", () => {
+    it('should be validated', () => {
+      expect(validateQueueName(new Array(25).join('a'))).toEqual([true])
+    })
+  })
+
+  describe('when one or more bad characters are given', () => {
+    it('should fail the validation', () => {
+      expect(validateQueueName('_')).toEqual([false, 'forbidden_chars'])
+      expect(validateQueueName('=')).toEqual([false, 'forbidden_chars'])
+      expect(validateQueueName('A')).toEqual([false, 'forbidden_chars'])
+      expect(validateQueueName('#')).toEqual([false, 'forbidden_chars'])
+    })
+  })
+
+  describe('when no bad characters are given', () => {
+    it('should be validated', () => {
+      expect(validateQueueName('queue-0')).toEqual([true])
     })
   })
 })
