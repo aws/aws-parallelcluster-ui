1221c9d294290208638bdb6343a03d4117429c31
Marco Basile <mrbarco@amazon.com>
2023-03-21 16:35:40 +0100

    useLoadingState hook implementation

---
+++ frontend/src/components/useLoadingState.tsx
@@ -0,0 +1,49 @@
+import {Box, SpaceBetween, Spinner} from '@cloudscape-design/components'
+import React from 'react'
+import {GetAppConfig, GetIdentity} from '../model'
+import {useTranslation} from 'react-i18next'
+
+export function LoadingSpinnerContent() {
+  const {t} = useTranslation()
+
+  return (
+    <SpaceBetween direction="vertical" size="l">
+      <Box textAlign="center">
+        <Spinner size="large"></Spinner>
+      </Box>
+    </SpaceBetween>
+  )
+}
+interface UseLoadingStateResponse {
+  loading: boolean
+  content: React.ReactNode
+}
+export function useLoadingState(
+  wrappedComponents: any,
+): UseLoadingStateResponse {
+  const [loading, setLoading] = React.useState(false)
+
+  React.useEffect(() => {
+    const getPreliminaryInfo = async () => {
+      setLoading(true)
+      await GetAppConfig()
+
+      try {
+        await GetIdentity(undefined, true)
+      } catch (error: any) {
+        const status = error?.response?.status
+        if (status != 403 && status != 401) {
+          throw error // rethrow in case error is not authn/z related
+        }
+      }
+      setLoading(false)
+    }
+
+    getPreliminaryInfo()
+  }, [])
+
+  return {
+    loading,
+    content: loading ? <LoadingSpinnerContent /> : wrappedComponents,
+  }
+}
