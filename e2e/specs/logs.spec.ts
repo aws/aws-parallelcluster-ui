feb8f1cc40727d534740152e5ed8eeea8425aca3
Alessandro Menduni <mendaomn@amazon.com>
2023-03-21 12:03:34 +0100

    Add e2e spec for the cluster logs page

---
+++ e2e/specs/logs.spec.ts
@@ -0,0 +1,30 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+// with the License. A copy of the License is located at
+//
+// http://aws.amazon.com/apache2.0/
+//
+// or in the "LICENSE.txt" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+// OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { test } from '@playwright/test';
+import { ClusterAction, selectCluster, selectClusterAction } from '../test-utils/clusters';
+import { visitAndLogin } from '../test-utils/login';
+import { visitClusterLogsPage } from '../test-utils/logs';
+
+test.describe('environment: @demo', () => {
+  test.describe('Given an endpoint where AWS ParallelCluster UI is deployed', () => {
+    test.describe('when the user selects a cluster', () => {
+      test('the user can navigate to the cluster logs page', async ({ page }) => {
+        await visitAndLogin(page)
+
+        await selectCluster(page)
+
+        await selectClusterAction(page, ClusterAction.VIEW_LOGS)
+
+        await visitClusterLogsPage(page)
+      });
+    })
+  })
+})
