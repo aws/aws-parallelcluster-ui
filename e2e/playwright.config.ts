ca2dd6af60bc1e10227c720d963cee99e0e077ae
Marco Basile <mrbarco@amazon.com>
2023-03-28 14:27:26 +0200

    fix for e2e

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -13,7 +13,7 @@ const config: PlaywrightTestConfig = {
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 10000
+    timeout: 30000
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -28,7 +28,7 @@ const config: PlaywrightTestConfig = {
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-    actionTimeout: 15000,
+    actionTimeout: 30000,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
 
