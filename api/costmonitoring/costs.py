5a9ebc6b4a69d07ee5078ecd1bcf35764653c9e6
Marco Basile <mrbarco@amazon.com>
2023-04-28 09:51:19 +0200

    added get_cost_data_for(cluster_name) implementation with cached response (to test with FE invocations)

--- api/costmonitoring/costs.py
+++ api/costmonitoring/costs.py
@@ -1,10 +1,17 @@
+from datetime import datetime, timedelta
+
 import boto3
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 
 from .costexplorer_client import CostExplorerClient, CostExplorerNotActiveException
 from ..PclusterApiHandler import authenticated
 from ..pcm_globals import logger
 from ..security.csrf.csrf import csrf_needed
+from ..utils import to_utc_datetime
+from ..validation import validated
+from ..validation.schemas import GetCostData
+
+CACHED_RESPONSE_MAX_AGE = 60 * 60 * 12
 
 COST_ALLOCATION_TAGS = ['parallelcluster:cluster-name']
 
@@ -27,6 +34,28 @@ def cost_monitoring_status():
 def activate_cost_monitoring():
     client.activate()
     return {}, 204
+
+
+@costs.get('/clusters/<cluster_name>')
+@authenticated({'admin'})
+@validated(params=GetCostData)
+def get_cost_data_for(cluster_name):
+    start = to_utc_datetime(request.args.get('start')).date().isoformat()
+    end = to_utc_datetime(request.args.get('end', default=datetime.today().isoformat())).date().isoformat()
+
+    cost_amounts = client.get_cost_data(cluster_name=cluster_name, start=start, end=end)
+
+    return __cached_response(jsonify({'costs': cost_amounts}), max_age=CACHED_RESPONSE_MAX_AGE)
+
+
+def __cached_response(response, max_age):
+    response.cache_control.max_age = max_age
+    response.cache_control.private = True
+    response.cache_control.immutable = True
+    response.last_modified = datetime.now()
+    response.expires = datetime.now() + timedelta(seconds=max_age)
+
+    return response
 
 
 @costs.errorhandler(CostExplorerNotActiveException)
